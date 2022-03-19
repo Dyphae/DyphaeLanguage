926d902a5216ca10f4ccaee0af5d8b60a6c1677d
MASTRIO <mastrio5772@gmail.com>
2022-03-19 15:49:46 +1000

    (very) basic datapack generation

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ pub const SUPPORTED_MINECRAFT_VERSIONS: [&str; 2] = [
 
 pub struct ProjectData {
     name: String,
+    namespace: String,
     description: String,
     minecraft_version: String,
     project_path: String
@@ -26,25 +27,28 @@ fn main() {
 fn process_args(args: Vec<String>) {
     if args[1] == "new" {
         println!("Creating new project with name '{}'", args[2]);
-
+        
+        /*
         let mut allow_version: bool = false;
         for version in SUPPORTED_MINECRAFT_VERSIONS {
             if args[3] == version {
                 allow_version = true;
             }
         }
+        */
 
-        if allow_version {
-            let project_data: ProjectData = ProjectData {
-                name: args[2].to_string(),
-                description: args[4].to_string(),
-                minecraft_version: args[3].to_string(),
-                project_path: args[5].to_string()
-            };
-            new_project::gen(project_data)
-        } else {
-            println!("Invalid minecraft version, please choose a supported version")
-        }
+        //if allow_version {
+        let project_data: ProjectData = ProjectData {
+            name: args[2].to_string(),
+            namespace: args[3].to_string(),
+            description: args[5].to_string(),
+            minecraft_version: args[4].to_string(),
+            project_path: args[6].to_string()
+        };
+        let _result = new_project::gen(project_data);
+        //} else {
+        //    println!("Invalid minecraft version '{}', please choose a supported version", args[4])
+        //}
     } else {
         println!("Unknown command: '{}'", args[1]);
     }
